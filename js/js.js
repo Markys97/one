e2c791ece7f2d4f00b6115f7eb152a9494b4bd22
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-05-31 13:09:52 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,19 @@
+let burgerBtn= document.querySelector('.header__burger ');
+let menu=  document.querySelector('.header__menu ');
+let btnCloseMenu=  document.querySelector('.menu__close-btn ');
+
+
+burgerBtn.addEventListener('click',e=>{
+    menu.classList.add('open-menu')
+   console.log(menu)
+})
+btnCloseMenu.addEventListener('click',e=>{
+    menu.classList.remove('open-menu')
+   console.log(menu)
+})
+
+window.addEventListener('resize',e=>{
+    if(window.screen.width> 1024){
+        menu.classList.remove('open-menu')
+    }
+})
